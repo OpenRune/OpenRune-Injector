c478952625b9636dde15f5d74a74d21abe728036
MatthewBishop <55411296+MatthewBishop@users.noreply.github.com>
2024-03-10 18:05:46 -0400

    Update injector to allow not using the gamepack

--- src/main/java/com/openosrs/injector/injection/InjectData.java
+++ src/main/java/com/openosrs/injector/injection/InjectData.java
@@ -10,6 +10,8 @@ package com.openosrs.injector.injection;
 import com.openosrs.injector.InjectUtil;
 import com.openosrs.injector.injectors.Injector;
 import com.openosrs.injector.rsapi.RSApi;
+
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.function.BiConsumer;
@@ -20,6 +22,10 @@ import net.runelite.asm.Field;
 import net.runelite.asm.Method;
 import net.runelite.asm.Type;
 import net.runelite.asm.signature.Signature;
+import net.runelite.asm.signature.util.VirtualMethods;
+import net.runelite.deob.DeobAnnotations;
+import net.runelite.deob.deobfuscators.Renamer;
+import net.runelite.deob.util.NameMappings;
 
 /**
  * Abstract class meant as the interface of {@link com.openosrs.injector.Injection injection} for injectors
@@ -47,6 +53,8 @@ public abstract class InjectData
 		this.rsApi = rsApi;
 		this.mixins = mixins;
 
+		removeRuneliteClasses();
+		remap();
 		initToVanilla();
 	}
 
@@ -65,6 +73,80 @@ public abstract class InjectData
 	public final Map<String, ClassFile> toDeob = new HashMap<>();
 
 	public abstract void runChildInjector(Injector injector);
+
+	private void removeRuneliteClasses() {
+		final var toRemove = new ArrayList<ClassFile>();
+		for (ClassFile classFile : this.vanilla) {
+			if (classFile.getName().contains("runelite")) {
+				toRemove.add(classFile);
+			}
+		}
+		toRemove.forEach(this.vanilla::removeClass);
+	}
+
+	private void remap() {
+		final var mappings = new NameMappings();
+		var nameIndex = 0;
+		for (ClassFile classFile : this.vanilla) {
+			for (Method method : classFile.getMethods()) {
+				final var nameAnn = method.getAnnotations().get(DeobAnnotations.OBFUSCATED_NAME);
+				if (nameAnn == null) {
+					continue;
+				}
+				var mapped = mappings.get(method.getPoolMethod());
+				if (mapped == null) {
+					mapped = "_" + nameIndex;
+					nameIndex++;
+				}
+				final var virtualMethods = VirtualMethods.getVirtualMethods(method);
+				for (Method virtualMethod : virtualMethods) {
+					final var deobAnnotation = this.deobfuscated
+							.findClass(virtualMethod.getClassFile().getName())
+							.findMethod(virtualMethod.getName(), virtualMethod.getDescriptor())
+							.getAnnotations().get(DeobAnnotations.OBFUSCATED_NAME);
+					deobAnnotation.setElement("value", mapped);
+					final var renamedName = deobAnnotation.getValueString();
+					mappings.map(virtualMethod.getPoolMethod(), renamedName);
+				}
+			}
+		}
+		final var renamer = new Renamer(mappings);
+		renamer.run(this.vanilla);
+
+		for (ClassFile classFile : this.deobfuscated) {
+			{
+				final var annotations = classFile.getAnnotations();
+				final var obfuscatedName = annotations.get(DeobAnnotations.OBFUSCATED_NAME);
+				if (obfuscatedName != null) {
+					obfuscatedName.setElement("value", classFile.getName());
+				}
+			}
+			{
+				for (Method method : classFile.getMethods()) {
+					final var annotations = method.getAnnotations();
+					final var obfuscatedSignature = annotations.get(DeobAnnotations.OBFUSCATED_SIGNATURE);
+					if (obfuscatedSignature != null) {
+						obfuscatedSignature.setElement("descriptor", method.getDescriptor().toString());
+					}
+				}
+			}
+			for (Field field : classFile.getFields()) {
+				final var annotations = field.getAnnotations();
+				final var obfuscatedName = annotations.get(DeobAnnotations.OBFUSCATED_NAME);
+				if (obfuscatedName != null) {
+					obfuscatedName.setElement("value", field.getName());
+				}
+				final var obfuscatedSignature = annotations.get(DeobAnnotations.OBFUSCATED_SIGNATURE);
+				if (obfuscatedSignature != null) {
+					obfuscatedSignature.setElement("descriptor", field.getType().toString());
+				}
+				final var obfuscatedGetter = annotations.get(DeobAnnotations.OBFUSCATED_GETTER);
+				if (obfuscatedGetter != null) {
+					annotations.remove(DeobAnnotations.OBFUSCATED_GETTER);
+				}
+			}
+		}
+	}
 
 	public void initToVanilla()
 	{
